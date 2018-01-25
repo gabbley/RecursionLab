53c09d174f748aadf92ab8aeaef8089f6f1c49f6
baniquedg <baniquedg@104-05-2017.bi.local>
2018-01-25 13:34:39 -0500

    Add fields

--- CookieMonster/src/State.java
+++ CookieMonster/src/State.java
@@ -1,4 +1,38 @@
 
 public class State {
 
+	private int row, col, val, sum;
+	private String path;
+	public State(int row, int col, int val, int sum, String path) {
+		super();
+		this.row = row;
+		this.col = col;
+		this.val = val;
+		this.sum = sum;
+		this.path = path;
+	}
+	
+	@Override
+	public String toString() {
+		return "State [row=" + row + ", col=" + col + ", val=" + val + ", sum=" + sum + ", path=" + path + "]";
+	}
+
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	public int getVal() {
+		return val;
+	}
+	public int getSum() {
+		return sum;
+	}
+	public String getPath() {
+		return path;
+	}
+	
+	
+	
 }
